3de0cc663344459587a88731037044b371c439f7
btgn <tbakula@calstatela.edu>
2021-01-28 21:56:18 -0600

    arrays and loops

---
+++ practice/js/arrays.js
@@ -0,0 +1,28 @@
+// Different Types of Arryas
+let intArr = [1, 2, 3];
+let stringArr = ["Good", "Morning", "People", "monkey", "cat", "dog"];
+let mixArr = [1, "good", 24.5];
+
+// Printing Arrays
+// console.log(intArr);
+// console.log(stringArr);
+// console.log(mixArr);
+
+// Array Operations
+let pets = ["bird", "cat", "dog", "ants", "monkey", "horse"];
+// what is the range of the array? 6 (Index starts from 0 and ends at 6)
+// console.log(pets.length); // what is the length of the array ? 7 (The number of elements in the array is 7)
+
+// pets.shift(); // deletes the first element of the array
+// pets.pop(); // deletes the last element of the array
+// pets.push("donkey", "horse"); // adds an element at the end of the array
+// pets.unshift("donkey"); // adds an element at the beginning of the array
+// pets[1] = "lizard"; changing the value of an element in the array
+// pets.slice(0, 3);
+// console.log(pets.slice(0, 3)); // deletes the selected elements from the array but returns a new array
+
+// pets.splice(3, 5); // deletes the selected elements from the original array
+
+console.log(pets);
+
+// Printing Arrays One by One
---
+++ practice/js/loops.js
@@ -0,0 +1,10 @@
+// FOR Loop
+for (let i = 1; i < 5; i++) {
+  // (initialization; condition; increment or decrement)
+  // console.log(i);
+}
+
+let x = 10;
+for (x; x >= 1; x--) {
+  console.log(x);
+}
