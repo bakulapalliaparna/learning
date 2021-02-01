87ccf69cb5e58b76121f2c125125080faeb5ae3c
btgn <tbakula@calstatela.edu>
2021-01-31 20:56:23 -0600

    strings and its methods

--- practice/js/arrays.js
+++ practice/js/arrays.js
@@ -8,7 +8,7 @@ let mixArr = [1, "good", 24.5];
 // console.log(stringArr);
 // console.log(mixArr);
 
-// Array Operations
+// Array Funcitons or Methods
 let pets = ["bird", "cat", "dog", "ants", "monkey", "horse"];
 // what is the range of the array? 6 (Index starts from 0 and ends at 6)
 // console.log(pets.length); // what is the length of the array ? 7 (The number of elements in the array is 7)
@@ -17,8 +17,9 @@ let pets = ["bird", "cat", "dog", "ants", "monkey", "horse"];
 // pets.pop(); // deletes the last element of the array
 // pets.push("donkey", "horse"); // adds an element at the end of the array
 // pets.unshift("donkey"); // adds an element at the beginning of the array
-// pets[1] = "lizard"; changing the value of an element in the array
-// pets.slice(0, 3);
+// pets[1] = "lizard"; // changing the value of an element in the array
+// let slice = pets.slice(0, 3);
+// pets.splice(0, 3);
 // console.log(pets.slice(0, 3)); // deletes the selected elements from the array but returns a new array
 
 // pets.splice(3, 5); // deletes the selected elements from the original array
---
+++ practice/js/strings.js
@@ -0,0 +1,23 @@
+let changed = "";
+let string = "tom and jerry";
+let trailSpaces = "    tom and jerry   ";
+let string1 = "tom,dick,harry";
+
+// changed = string.toUpperCase(); // to convert the string to uppercase
+// changed = string.toLowerCase(); // to convert the string to lowercase
+// changed = string.replace("tom", "Tom"); // replaces a single word in the given value
+// changed = string.length; // displays the length of the given string
+// changed = string.split(); // splits the string into an array
+// changed = string.split(" "); // splits the string into an array based on the delimiter given
+// changed = string1.split(","); // splits the string into an array based on the delimiter given
+// changed = string.charAt(0); // treats the entire string as an array and defines each letter as an index
+// changed = string.concat(" and spike"); // concats the given parameter to the original value
+// changed = string.startsWith("tom"); // checks if the given string starts with the given parameter
+// changed = string.endsWith("jerry"); // checks if the given string ends with the given parameter
+// changed = string.includes("errs"); // checks if the given parameter is part of the original value or not
+// changed = string.slice(4, 7); // slices the given value to the given parameters
+// changed = string.substring(0, 5); // slices the given value to the given parameters
+// changed = trailSpaces.trim(); // removes all the trailing spaces both front and back for the given word
+// changed = string.indexOf("i"); // displays the index of the first letter of the given parameter, if the parameter does not exist in the given word then, it returns a negative value
+
+console.log(changed);
